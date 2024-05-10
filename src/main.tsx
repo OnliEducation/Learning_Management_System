0722fd67c6d7a7b74e2d390d0ccea7b09df77939
Kirill Shulgin <mister.fedos2011@yandex.ru>
2024-05-10 22:06:14 +0200

    change directory

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles/index.css'
 
-import Arrow from '../shared/ui/icons/arrow.svg?react'
-import { Button } from '../shared/ui/components/Button'
-import { Input } from '../shared/ui/components/Input'
+import Arrow from './shared/ui/icons/arrow.svg?react'
+import { Button } from './shared/ui/components/Button'
+import { Input } from './shared/ui/components/Input'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
